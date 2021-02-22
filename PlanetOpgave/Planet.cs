d816cfeb1fed19fcf6775a9ae0c2617e5acd3c47
patrick jesssen <patrick-j1@hotmail.dk>
2021-02-22 10:51:17 +0100

    Add project files.

---
+++ PlanetOpgave/Planet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetOpgave
+{
+    class Planet
+    {
+        public string Name { get; set; }
+        public double Mass { get; set; }
+        public double Diameter { get; set; }
+        public double Density { get; set; }
+        public double Gravity { get; set; }
+        public TimeSpan RotationPeriod { get; set; }
+        public TimeSpan LengthOfDay { get; set; }
+        public double DistanceFromSun { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+        public double OrbitalVelocity { get; set; }
+        public double MeanTemperature { get; set; }
+        public int NumberOfMoons { get; set; }
+        public bool HasRingSystem { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Name of the planet</param>
+        /// <param name="mass">Mass of the planet</param>
+        /// <param name="diameter">Diameter of the planet</param>
+        /// <param name="density">Density of the planet</param>
+        /// <param name="gravity">Gravity of the planet</param>
+        /// <param name="rotationPeriod">Rotation time of the planet</param>
+        /// <param name="lengthOfDay">How long is a day on that planet</param>
+        /// <param name="distanceFromSun">Distance from planet to the sun</param>
+        /// <param name="orbitalPeriod">Orbital Period on the planet</param>
+        /// <param name="orbitalVelocity">Orbital Velocity of the planet</param>
+        /// <param name="meanTemperature">Temperature on the planet</param>
+        /// <param name="numberOfMoons">How many moons does the planet have</param>
+        /// <param name="hasRingSystem">Does the planet have any ring systems?</param>
+        public Planet(string name, double mass, double diameter, double density, double gravity, TimeSpan rotationPeriod, TimeSpan lengthOfDay, double distanceFromSun, TimeSpan orbitalPeriod, double orbitalVelocity, double meanTemperature, int numberOfMoons, bool hasRingSystem)
+        {
+            Name = name;
+            Mass = mass;
+            Diameter = diameter;
+            Density = density;
+            Gravity = gravity;
+            RotationPeriod = rotationPeriod;
+            LengthOfDay = lengthOfDay;
+            DistanceFromSun = distanceFromSun;
+            OrbitalPeriod = orbitalPeriod;
+            OrbitalVelocity = orbitalVelocity;
+            MeanTemperature = meanTemperature;
+            NumberOfMoons = numberOfMoons;
+            HasRingSystem = hasRingSystem;
+
+        }
+    }
+}
---
+++ PlanetOpgave/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Planet> planets = new List<Planet>();
+            List<Planet> planetsBellowZero = new List<Planet>();
+            List<Planet> PlanetsWithDiameter = new List<Planet>();
+            planets.Add(new Planet("Mekur", 0.330, 4879, 5427, 3.7, TimeSpan.FromHours(1407.6), TimeSpan.FromHours(4222.6), 57.9, TimeSpan.FromDays(88.0), 47.4, 167, 0, false));
+            planets.Add(new Planet("Venus", 4.87, 12.104, 5243, 8.9, TimeSpan.FromHours(-5832.5), TimeSpan.FromHours(2802.0), 108.2, TimeSpan.FromDays(224.7), 35.0, 464, 0, false));
+            planets.Add(new Planet("Earth", 5.97, 12.756, 5514, 9.8, TimeSpan.FromHours(23.9), TimeSpan.FromHours(24.0), 149.6, TimeSpan.FromDays(365.2), 29.8, 15, 1, false));
+            planets.Add(new Planet("Mars", 0.642, 6792, 3933, 3.7, TimeSpan.FromHours(24.6), TimeSpan.FromHours(24.7), 227.9, TimeSpan.FromDays(687.0), 24.1, -65, 2, false));
+            planets.Add(new Planet("Jupiter", 1898, 142.984, 1326, 23.1, TimeSpan.FromHours(9.9), TimeSpan.FromHours(9.9), 778.6, TimeSpan.FromDays(4331), 13.1, -110, 67, true));
+            planets.Add(new Planet("Saturn", 568, 120.536, 687, 9.0, TimeSpan.FromHours(10.7), TimeSpan.FromHours(10.7), 1433.5, TimeSpan.FromDays(10.747), 9.7, -140, 62, true));
+            planets.Add(new Planet("Uranus", 86.8, 51.118, 1271, 8.7, TimeSpan.FromHours(-17.2), TimeSpan.FromHours(17.2), 2872.5, TimeSpan.FromDays(30.589), 6.8, -195, 27, true));
+            planets.Add(new Planet("Neptun", 102, 49.528, 1638, 11.0, TimeSpan.FromHours(16.1), TimeSpan.FromHours(16.1), 4495.1, TimeSpan.FromDays(58.8), 5.4, -200, 14, true));
+            planets.Add(new Planet("Pluto", 0.0146, 2370, 2095, 0.7, TimeSpan.FromHours(-153.3), TimeSpan.FromHours(153.3), 5906.4, TimeSpan.FromDays(90.56), 4.7, -225, 5, false));
+
+            for (int i = 0; i < planets.Count; i++)
+            {
+                if (planets[i].Name == "Pluto")
+                {
+                    planets.RemoveAt(i);
+                }
+            }
+
+            planets.Add(new Planet("Pluto", 0.0146, 2370, 2095, 0.7, TimeSpan.FromHours(-153.3), TimeSpan.FromHours(153.3), 5906.4, TimeSpan.FromDays(90.56), 4.7, -225, 5, false));
+            foreach (Planet planet in planets)
+            {
+                if (planet.HasRingSystem == true)
+                {
+                    Console.WriteLine($"{planet.Name}, {planet.Mass}, {planet.Diameter}, {planet.Density}, {planet.Gravity}, {planet.RotationPeriod}, {planet.LengthOfDay}, {planet.DistanceFromSun}, {planet.OrbitalPeriod}, {planet.OrbitalVelocity}, {planet.MeanTemperature}, {planet.NumberOfMoons}, Yes");
+                }
+                else
+                {
+                    Console.WriteLine($"{planet.Name}, {planet.Mass}, {planet.Diameter}, {planet.Density}, {planet.Gravity}, {planet.RotationPeriod}, {planet.LengthOfDay}, {planet.DistanceFromSun}, {planet.OrbitalPeriod}, {planet.OrbitalVelocity}, {planet.MeanTemperature}, {planet.NumberOfMoons}, No");
+                }
+                if (planet.MeanTemperature <= 0)
+                {
+                    planetsBellowZero.Add(planet);
+                }
+                if (planet.Diameter > 10000 && planet.Diameter < 50000)
+                {
+                    PlanetsWithDiameter.Add(planet);
+                }
+            }
+            for (int i = 0; i < planetsBellowZero.Count; i++)
+            {
+                Console.WriteLine(planetsBellowZero[i].Name);
+            }
+            Console.WriteLine(planets.Count);
+            for (int i = 0; i < planets.Count; i++)
+            {
+                planets.RemoveAt(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
